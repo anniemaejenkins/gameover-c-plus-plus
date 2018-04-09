7306de554767a7dc6888caa1a19bcce485e90f42
Annie-mae Jenkins <daggerarcadia@gmail.com>
2018-04-09 01:55:09 -0400

    demonstrate using declarations

--- gameover.cpp
+++ gameover.cpp
@@ -20,3 +20,18 @@
 //   return 0;
 // }
 // adding the using namespace std; keeps you from having to type the std:: prefix
+
+
+//  Demonstrate using declarations
+
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+  cout << "Game Over!" << endl;
+  return 0;
+}
+
+// Able to access which elements you want from the std namespace and clarifies which elements you plan to use
